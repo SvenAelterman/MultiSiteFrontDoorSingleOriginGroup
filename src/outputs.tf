4c7f49689fcf1326b1a30b956b00b593d6eebbdd
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2025-10-06 19:24:30 -0500

    Remove debug output

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,6 +1,6 @@
-output "all_domain_names" {
-  value = local.all_domain_names
-}
+# output "all_domain_names" {
+#   value = local.all_domain_names
+# }
 
 # output "all_origins" {
 #   value = local.all_origins_map
